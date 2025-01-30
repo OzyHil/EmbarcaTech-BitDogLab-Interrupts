8f34e5cddb8c2b00db55d9bebac0e20331823b61
Hilquias Rodrigues de Oliveira <eaeamigo2292@gmail.com>
2025-01-30 00:05:01 -0300

    GPIO 5 configuration and button interrupt A

--- BitDogLab_Interrupts.c
+++ BitDogLab_Interrupts.c
@@ -2,6 +2,11 @@
 #include "pico/stdlib.h"
 
 #define RED_LED 13
+#define BUTTON_A 5
+
+static void ButtonAInterrupt(uint gpio, uint32_t events){
+    printf("A\n");
+}
 
 int main()
 {
@@ -10,8 +15,14 @@ int main()
     gpio_init(RED_LED);
     gpio_set_dir(RED_LED, GPIO_OUT);
 
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, 1, & ButtonAInterrupt);
+
     while (true) {
         gpio_put(RED_LED, !gpio_get(RED_LED));
         sleep_ms(100);
+
     }
 }
